1a3f5343ea2c21e795bdc24e4eb30aa7e0de0781
Mihlali <4280734@myuwc.ac.za>
2022-04-25 15:37:42 +0200

    added an exit condition (while)

--- Hangman.Core/Game/Hangman.cs
+++ Hangman.Core/Game/Hangman.cs
@@ -8,94 +8,114 @@ namespace Hangman.Core.Game
         private GallowsRenderer _renderer;
         private string _Chosenword;
         private string guess;
-        private int index;
+        //private int index;
         private int lives;
         private string _dashhyphen;
         private char[] overWritehyp;
        // private string _guessprogress;
-        public string Dash()
+      
+
+        public HangmanGame()
+        {
+            _renderer = new GallowsRenderer();
+
+            string[] MovieList =new string[] { "captain america", "iron man", "spider man", "captain hook", "infinite", "restless", "black crab", "the batman", "shang chi", "free guy", "snake eyes", "tomb raider", "spider man", "uncharted", "deadpool", "bloodshot", "mortal kombat", "the adam project" };
+            int index = new Random().Next(MovieList.Length);
+            _Chosenword = MovieList[index];
+            
+            for (int i = 0; i < _Chosenword.Length; i++)
+            {
+                _dashhyphen += "_";//console.writeline
+            }
+            
+        }
+
+        public string Returndash()
         {
             return _dashhyphen;
         }
 
-        public HangmanGame()
+        public bool Overwrite(char guess)
         {
 
-            string[] MovieList =new string[] { "captain america", "iron man", "spider man", "captain hook", "infinite", "restless", "black crab", "the batman", "shang chi", "free guy", "snake eyes", "tomb raider", "spider man", "uncharted", "deadpool", "bloodshot", "mortal kombat", "the adam project" };
-            index = new Random().Next(MovieList.Length);
-            _Chosenword = MovieList[index];
-            for (int i = 0; i < MovieList.Length; i++)
-            {
-                _dashhyphen += "_";
-            }
-        
             
-            {
-                _renderer = new GallowsRenderer();
-            }
-
-        }
-        
-        public void Overwrite(char guess)
-        {
-
-            _dashhyphen = new string(overWritehyp);// this may be wrong
-            {
                 overWritehyp = _dashhyphen.ToCharArray();
-            }
+
+            bool correct = false;            
             for (int i = 0; i < _Chosenword.Length; i++)
             {
-                if (_Chosenword[index] == guess)
+                if (_Chosenword[i] == guess)
                 {
-                    overWritehyp[index] = guess;
+                    overWritehyp[i] = guess;
+
+                    correct = true;
+                    // guess correct
+
+
                 }
+
             }
 
+            _dashhyphen = new string(overWritehyp);
+
+            return correct;
         }
-            public void Run()
+        public void Run()
         {
             lives = 6;
+            //for (int lives = 6; 0 < lives ; lives--)
 
-            while (true)
+            bool won = false;
+
+            while (lives > 0 && !won)
             {
                 _renderer.Render(5, 5, lives);
+            
 
                 Console.SetCursorPosition(0, 13);
                 Console.ForegroundColor = ConsoleColor.Blue;
                 Console.Write("Your current guess: ");
-                
+
                 Console.SetCursorPosition(0, 15);
+
+                Console.Write(_dashhyphen);
 
                 Console.ForegroundColor = ConsoleColor.Green;
 
-               
+                
+
                 Console.Write("What is your next guess: ");
-                var nextGuess = Console.ReadLine();
                 guess = Console.ReadLine();
                 guess = guess.ToLower();
-                Console.WriteLine("                                    ");
-                
-                
-            }
-            if (_Chosenword.Contains(guess))
-            {
+
+                bool correctGuess = Overwrite(guess[0]);
+
+//                guess = Console.ReadLine();
+  //              guess = guess.ToLower();
+    //            Console.WriteLine(" ");
+
+               
+                if (!correctGuess)
+                    lives--;
+
+                if (_Chosenword == _dashhyphen)
+                {
+                    won = true;
+                }
 
             }
-            else if (!_Chosenword.Contains(guess))
-            {
-                lives--;
 
-                
-            }
             if (lives == 0)
             {
                 Console.WriteLine("You have died :(");
             }
-            //else (lives != 0)
-                    {
+            else 
+            {
                 Console.WriteLine("You have survived :)");
-                    }
-        }
+            }
 
+
+
+        }
     }
 }
